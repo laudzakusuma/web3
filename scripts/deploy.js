2d2587eb9c6d63daee7984277768505b7da65d0e
laudzakusuma <laudzaxie@gmail.com>
2025-07-30 13:13:21 +0700

    s

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,15 +1,18 @@
 const hre = require("hardhat");
 
 async function main() {
-    const Crowdfunding = await hre.ethers.getContractFactory("Crowdfunding");
-    const crowdfunding = await Crowdfunding.deploy();
+  const Crowdfunding = await hre.ethers.getContractFactory("Crowdfunding");
+  
+  console.log("Deploying contract...");
 
-    await crowdfunding.getDeployedCode();
+  const crowdfunding = await Crowdfunding.deploy();
+  
+  await crowdfunding.deployed();
 
-    console.log(`Contract deployed to ${crowdfunding.address}`);
+  console.log(`Contract deployed to: ${crowdfunding.address}`);
 }
 
 main().catch((error) => {
-    console.error(error);
-    process.exitCode = 1;
+  console.error(error);
+  process.exitCode = 1;
 });
