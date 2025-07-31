68cb5342c2961ce67944d791a19ebb7ecc4cbffb
laudzakusuma <laudzaxie@gmail.com>
2025-07-31 17:39:04 +0700

    upgrade

--- contracts/Crowdfunding.sol
+++ contracts/Crowdfunding.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity^0.8.23;
+pragma solidity ^0.8.9;
 
 contract Crowdfunding {
     struct Campaign {
@@ -12,6 +12,7 @@ contract Crowdfunding {
         string image;
         address[] donators;
         uint256[] donations;
+        bool paidOut;
     }
 
     mapping(uint256 => Campaign) public campaigns;
@@ -19,7 +20,6 @@ contract Crowdfunding {
 
     function createCampaign(address _owner, string memory _title, string memory _description, uint256 _target, uint256 _deadline, string memory _image) public returns (uint256) {
         Campaign storage campaign = campaigns[numberOfCampaigns];
-        require(campaign.deadline == 0, "Campaign already exists.");
         campaign.owner = _owner;
         campaign.title = _title;
         campaign.description = _description;
@@ -27,19 +27,35 @@ contract Crowdfunding {
         campaign.deadline = _deadline;
         campaign.amountCollected = 0;
         campaign.image = _image;
+        campaign.paidOut = false;
+
         numberOfCampaigns++;
+
         return numberOfCampaigns - 1;
     }
 
     function donateToCampaign(uint256 _id) public payable {
         uint256 amount = msg.value;
         Campaign storage campaign = campaigns[_id];
+
+        require(block.timestamp < campaign.deadline, "The deadline has passed");
+
         campaign.donators.push(msg.sender);
         campaign.donations.push(amount);
-        (bool sent,) = payable(campaign.owner).call{value: amount}("");
-        if (sent) {
-            campaign.amountCollected = campaign.amountCollected + amount;
-        }
+        campaign.amountCollected = campaign.amountCollected + amount;
+    }
+
+    function payoutToOwner(uint256 _id) public {
+        Campaign storage campaign = campaigns[_id];
+
+        require(msg.sender == campaign.owner, "You are not the owner of this campaign.");
+        require(block.timestamp >= campaign.deadline, "The deadline has not yet passed.");
+        require(!campaign.paidOut, "Funds have already been paid out.");
+
+        campaign.paidOut = true;
+
+        (bool sent, ) = payable(campaign.owner).call{value: campaign.amountCollected}("");
+        require(sent, "Failed to send funds");
     }
 
     function getDonators(uint256 _id) public view returns (address[] memory, uint256[] memory) {
@@ -50,7 +66,7 @@ contract Crowdfunding {
         Campaign[] memory allCampaigns = new Campaign[](numberOfCampaigns);
         for (uint i = 0; i < numberOfCampaigns; i++) {
             Campaign storage item = campaigns[i];
-            allCampaigns[i]= item;
+            allCampaigns[i] = item;
         }
         return allCampaigns;
     }
